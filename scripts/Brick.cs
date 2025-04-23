b80086dc3f7f882cce32c24a9eeb70c3d24492bd
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2025-04-23 21:54:30 +0800

    feat: implement brick breaking sound

--- scripts/Brick.cs
+++ scripts/Brick.cs
@@ -11,8 +11,15 @@ public partial class Brick : StaticBody2D
 	{
 	}
 
-    // this method is called by the ball when it hits a brick
+	// this method is called by the ball when it hits a brick
 	private void OnHitByBall()
+	{
+		Hide(); // make brick invisible while keeping node so audio can play
+		GetNode<AudioStreamPlayer2D>("audio_break").Play(); // play brick breaking sound
+	}
+
+	// this method is called after the breaking sound finishes playing
+	private void OnAudioBreakFinished()
 	{
 		QueueFree(); // delete brick from scene
 	}
